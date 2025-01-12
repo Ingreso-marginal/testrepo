c84ef5853394c9c38b19d4ae0eeda7824654d2aa
Ingreso-marginal <ingreso.marginal@gmail.com>
2025-01-12 18:53:32 +0100

    Add files via upload

---
+++ 00_hello.py
@@ -0,0 +1,15 @@
+# Esto es un comenario:
+# Nuestro Hola mundo en Python
+print("Hola Python")
+
+"""
+Este es un
+comentario
+en varias líneas
+"""
+
+# Consulta tipo de dato
+print(type("Hola Python")) # Tipo "str"
+print(type(5)) # Tipo "int"
+print(type(1.5)) # Tipo "float"
+
